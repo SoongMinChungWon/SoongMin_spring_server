01afb713a732d12f507dabdb0d303111e15d5927
woohyuk <7dngur7@naver.com>
2024-07-24 16:15:59 +0900

    first setting

---
+++ ssupetition/src/main/java/com4table/ssupetition/SsupetitionApplication.java
@@ -0,0 +1,13 @@
+package com4table.ssupetition;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SsupetitionApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SsupetitionApplication.class, args);
+	}
+
+}
